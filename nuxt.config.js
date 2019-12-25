4f1f0bcb320c76b7dcee1bfcfa9f6d58ee2df09e
Mindochin <mindochin@yandex.ru>
2019-12-25 12:33:13 +0500

    pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
   },
   toast: {
     position: 'top-center',
